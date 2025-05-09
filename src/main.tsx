4634fc571af1858a55f9bf1e1527dcfc67de94fb
eth6048 <theom@allmytour.com>
2025-05-09 16:17:24 +0900

    test : 실패 케이스 테스트 커밋

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 // import abc;
-// import test;
+import test;
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
